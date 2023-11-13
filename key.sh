148afcd27b288989b3ff304ee1fbc43bbe73f869
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-13 11:23:35 -0600

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,1 +1,5 @@
+
+curl -fsSL https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/rabbitmq-archive-keyring.gpg
+
+
 sudo apt-key adv --keyserver "hkps://keys.openpgp.org" --recv-keys "0x0A9AF2115F4687BD29803A206B73A36E6026DFCA"
