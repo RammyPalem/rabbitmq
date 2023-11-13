62c2013f63379b78b2b1d65efc69c9469cdc5dab
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-13 11:24:17 -0600

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,5 +1,5 @@
 
 curl -fsSL https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/rabbitmq-archive-keyring.gpg
 
-
+echo "deb [signed-by=/usr/share/keyrings/rabbitmq-archive-keyring.gpg] https://dl.bintray.com/rabbitmq/debian focal main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 sudo apt-key adv --keyserver "hkps://keys.openpgp.org" --recv-keys "0x0A9AF2115F4687BD29803A206B73A36E6026DFCA"
